32d2d02e062db9afd8d2214280a5d8b5a562eb56
jiro <jiroron666@gmail.com>
2017-08-25 21:52:14 +0900

    go言語の設定を追加。

---
+++ setup_go.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+sudo apt-get install -y golang
+sudo apt-get install libgtk2.0-dev libglib2.0-dev libgtksourceview2.0-dev -y
+go get github.com/mattn/go-gtk/gtk
+go install github.com/mattn/go-gtk/gtk
