a188fedac4ae0dc772b3b0a05819c63d795826af
jiro4989 <jiroron666@gmail.com>
2017-11-14 00:02:43 +0900

    Kotlinのインストールコマンドを追加

--- setup_java.sh
+++ setup_java.sh
@@ -9,3 +9,8 @@ sudo apt-get install ant -y
 sudo apt-get install maven -y
 sudo apt-get install openjdk-8-jdk -y
 sudo apt-get install openjfx -y
+
+# Kotlinのインストール
+curl -s http://get.sdkman.io | bash
+source $HOME/.sdkman/bin/sdkman-init.sh
+sdk install kotlin
