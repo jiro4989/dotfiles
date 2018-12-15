f8c8e261e18ca002299b0cde0a43d9285820c8c0
jiro4989 <jiroron666@gmail.com>
2018-12-15 15:45:28 +0900

    :sparkles:ヒストリ設定をファイルに分離した

---
+++ zsh/conf.d/history.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+export HISTFILE=${HOME}/.zsh_history
+
+# メモリに保存される履歴の件数
+export HISTSIZE=1000
+
+# 履歴ファイルに保存される履歴の件数
+export SAVEHIST=100000
+
+# 重複を記録しない
+setopt hist_ignore_dups
+
+# 開始と終了を記録
+setopt EXTENDED_HISTORY
+
