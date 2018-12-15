bd4bf36cbb25a72bfcb4b34b086f4e99ba7a5c66
jiro4989 <jiroron666@gmail.com>
2018-12-15 22:34:47 +0900

    :sparkles:プロンプト表示の色合いを変更

--- zsh/conf.d/prompt.zsh
+++ zsh/conf.d/prompt.zsh
@@ -9,5 +9,9 @@ zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
 # %F{色} 任意のテキスト %fで、%fまで色を変更できる
-PROMPT='%F{yellow}%*%f %F{magenta}%n%f@%F{blue}%M%f %{${fg[blue]}%}[%~]%{${reset_color}%}${vcs_info_msg_0_}
+PROMPT='%F{003}%*%f %F{magenta}%n%f@%F{blue}%M%f %F{200}[%~]%f${vcs_info_msg_0_}
 %# '
+
+show_zsh_prompt_colors() {
+  for c in {000..255}; do echo -n "\e[38;5;${c}m $c" ; [ $(($c%16)) -eq 15 ] && echo;done;echo
+}
