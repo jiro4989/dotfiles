1cbefbb7e02ee3c7b56cace49d3a10b43660ef4e
jiro4989 <jiroron666@gmail.com>
2018-12-24 11:50:47 +0900

    :fire:不要になったので削除

--- setup/in_pkgs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install dselect
-sudo dselect update
-sudo dpkg --clear-selections
-sudo dpkg --set-selections < ubuntu/pkgs.list
-sudo apt-get dselect-upgrade
-sudo apt-get autoremove
--- setup/out_pkgs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dpkg --get-selections > ubuntu/pkgs.list
