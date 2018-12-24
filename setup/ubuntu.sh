a3773af6d52b0a5a19cc9f1c27b84a12a121bc0f
jiro4989 <jiroron666@gmail.com>
2018-12-24 10:07:15 +0900

    :recycle:関数化した

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,7 +1,61 @@
 #!/bin/bash
 
-setup_general() {
-  # 全般{{{
+sub_cmd=$1
+
+export GOPATH=$HOME
+export PATH=$PATH:$HOME/bin
+my_repo=~/src/github.com/jiro4989
+dot_dir="$my_repo/dotfiles"
+
+main() {#{{{
+  # ヘルプオプションチェック
+  case "$sub_cmd" in
+    -h | --help | help)
+      usage
+      ;;
+  esac
+
+  # ヘルプの確認のときはデバッグ出力してほしくないので
+  # このタイミングでオプションをON
+  set -eux
+
+  case "_$sub_cmd" in
+    _general) setup_general ;;
+    _ghq) setup_ghq ;;
+    _shell) setup_shell ;;
+    _git_config) setup_git_config ;;
+    _docker) setup_docker ;;
+    _emacs) setup_emacs ;;
+    _go) setup_go ;;
+    _gtk_theme) setup_gtk_theme ;;
+    _vim) setup_vim ;;
+    _nvim) setup_nvim ;;
+    _atom) setup_atom ;;
+    _)
+      sudo apt update -y
+
+      setup_general
+      setup_ghq
+      setup_shell
+      setup_git_config
+      setup_docker
+      setup_emacs
+      setup_go
+      setup_gtk_theme
+      setup_vim
+      setup_nvim
+      setup_atom
+
+      sudo apt upgrade -y
+      ;;
+    *)
+      usage
+      ;;
+  esac
+}#}}}
+
+setup_general() {#{{{
+  # 全般
   # ユーザフォルダの日本語を英語に変更
   LANG=C xdg-user-dirs-gtk-update
 
@@ -19,37 +73,33 @@ setup_general() {
 
   # 日本語入力
   sudo apt-get install -y ibus-mozc
-  #}}}
-}
+}#}}}
 
-setup_ghq() {
-  # ghqでdotfilesを取得 {{{
+setup_ghq() {#{{{
+  # ghqでdotfilesを取得
   sudo apt install -y golang-1.10
   go get github.com/motemen/ghq
   ghq get jiro4989/dotfiles
-  #}}}
-}
+}#}}}
 
-setup_shell() {
-  # bash/zsh{{{
+setup_shell() {#{{{
+  # bash/zsh
   echo "source $dot_dir/bash/bashrc" >>~/.bashrc
   echo "source $dot_dir/zsh/zshrc" >>~/.zshrc
-  #}}}
-}
+}#}}}
 
-setup_git_config() {
-  # git config{{{
+setup_git_config() {#{{{
+  # git config
   git config --global user.email "jiroron666@gmail.com"
   git config --global user.name "jiro4989"
   git config --global alias.pr "pull-request"
   git config --global alias.see "browse"
   git config --global ghq.root "~/src"
   git config --global commit.template "$dot_dir/git/commit.template"
-  #}}}
-}
+}#}}}
 
-setup_docker() {
-  # docker{{{
+setup_docker() {#{{{
+  # docker
   sudo apt install docker.io
 
   sudo groupadd docker
@@ -59,19 +109,17 @@ setup_docker() {
   sudo chmod +x /usr/local/bin/docker-compose
 
   sudo systemctl enable docker
-  #}}}
-}
+}#}}}
 
-setup_emacs() {
-  # emacs{{{
+setup_emacs() {#{{{
+  # emacs
   sudo apt-get install -y emacs
   git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
   ln -sf $HOME/dotfiles/.spacemacs $HOME/.emacs.d/
-  #}}}
-}
+}#}}}
 
-setup_go() {
-  # go{{{
+setup_go() {#{{{
+  # go
   go get -u github.com/cweill/gotests/...
   go get -u github.com/mholt/archiver/cmd/archiver
 
@@ -126,11 +174,10 @@ setup_go() {
 
   # リポジトリ管理
   go get github.com/motemen/ghq
-  #}}}
-}
+}#}}}
 
-setup_gtk_theme() {
-  # gtk theme{{{
+setup_gtk_theme() {#{{{
+  # gtk theme
   git clone https://github.com/tliron/install-gnome-themes /tmp/install-gnome-themes
   sudo apt install ruby
   /tmp/install-gnome-themes/install-gnome-themes
@@ -142,37 +189,33 @@ setup_gtk_theme() {
 
   # app thems
   sudo apt install materia-gtk-theme
-  #}}}
-}
+}#}}}
 
-setup_vim() {
-  # vim{{{
+setup_vim() {#{{{
+  # vim
   sudo apt install vim
   sudo apt install vim-gnome
   ln -snf "$dot_dir/vim" $HOME/.vim
-  #}}}
-}
+}#}}}
 
-setup_nvim() {
-  # nvim{{{
+setup_nvim() {#{{{
+  # nvim
   curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
   chmod u+x nvim.appimage
   sudo mv nvim.appimage /usr/bin/nvim
   mkdir -p $HOME/.config
   ln -snf "$dot_dir/vim" "$HOME/.config/nvim"
-  #}}}
-}
+}#}}}
 
-setup_atom() {
-  # atom{{{
+setup_atom() {#{{{
+  # atom
   sudo add-apt-repository ppa:webupd8team/atom
   sudo apt-get update
   sudo apt-get upgrade -y
   sudo apt-get install -y atom
-  #}}}
-}
+}#}}}
 
-usage() {
+usage() {#{{{
   script_name=$(basename $0)
   cat <<EOS
 $script_name is script to setup OS.
@@ -187,56 +230,6 @@ usage:
     $script_name
 EOS
   exit 1
-}
+}#}}}
 
-sub_cmd=$1
-
-# ヘルプオプションチェック
-case "$sub_cmd" in
-  -h|--help|help)
-    usage
-    ;;
-esac
-
-# ヘルプの確認のときはデバッグ出力してほしくないので
-# このタイミングでオプションをON
-set -eux
-
-export GOPATH=$HOME
-export PATH=$PATH:$HOME/bin
-my_repo=~/src/github.com/jiro4989
-dot_dir="$my_repo/dotfiles"
-
-case "_$sub_cmd" in
-_general) setup_general ;;
-_ghq) setup_ghq ;;
-_shell) setup_shell ;;
-_git_config) setup_git_config ;;
-_docker) setup_docker ;;
-_emacs) setup_emacs ;;
-_go) setup_go ;;
-_gtk_theme) setup_gtk_theme ;;
-_vim) setup_vim ;;
-_nvim) setup_nvim ;;
-_atom) setup_atom ;;
-_)
-  sudo apt update -y
-
-  setup_general
-  setup_ghq
-  setup_shell
-  setup_git_config
-  setup_docker
-  setup_emacs
-  setup_go
-  setup_gtk_theme
-  setup_vim
-  setup_nvim
-  setup_atom
-
-  sudo apt upgrade -y
-  ;;
-*)
-  usage
-  ;;
-esac
+main
