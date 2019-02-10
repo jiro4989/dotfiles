2432adc3b2922fbdcc192c11189d593bfb17a61a
jiro4989 <jiroron666@gmail.com>
2019-02-10 16:20:05 +0900

    :sparkles:Ansible2.7をインストールスクリプトを追加した

--- setup/dependencies.sh
+++ setup/dependencies.sh
@@ -6,3 +6,4 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible-2.7
 sudo apt-get install ansible -y
+sudo apt-get upgrade
