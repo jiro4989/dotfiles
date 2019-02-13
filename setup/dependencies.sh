31f702a478a270fe8ddb5f0822383cb8ff69d4f6
jiro4989 <jiroron666@gmail.com>
2019-02-13 23:10:51 +0900

    :fire:setupを専用リポジトリに分離したので削除

--- setup/dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible-2.7
-sudo apt-get install ansible -y
-sudo apt-get upgrade
