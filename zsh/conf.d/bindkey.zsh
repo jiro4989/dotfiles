05a938c369b6c2675cb83b4de95dfdcfb9f76cf2
jiro4989 <jiroron666@gmail.com>
2019-06-27 12:09:36 +0900

    :wrench: peco cd and peco editを追加した

--- zsh/conf.d/bindkey.zsh
+++ zsh/conf.d/bindkey.zsh
@@ -22,3 +22,23 @@ bindkey "^[[1~" beginning-of-line
 bindkey "^[[3~" delete-char
 # END
 bindkey "^[[4~" end-of-line
+
+# findの結果をpecoで選択し、ディレクトリを移動する
+__peco_cd() {
+  local __dir=$(find ./ -path '*/\.*' -name .git -prune -o -type d -print 2> /dev/null | peco)
+  if [ -n "$__dir" ]; then
+    cd $__dir
+  fi
+}
+zle -N __peco_cd
+alias pd=__peco_cd
+
+# findの結果をpecoで選択し、vimで編集する
+__peco_edit() {
+  local __file=$(find ./ -path '*/\.*' -name .git -prune -o -type f -print 2> /dev/null | peco)
+  if [ -n "$__file" ]; then
+    ${EDITOR:-vim} $__file
+  fi
+}
+zle -N __peco_edit
+alias pe=__peco_edit
