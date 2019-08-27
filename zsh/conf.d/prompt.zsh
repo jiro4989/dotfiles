9f517b9ef9e8a859d617a722a85fd3f9084f08ed
jiro4989 <jiroron666@gmail.com>
2019-08-27 23:35:07 +0900

    :sparkles: プロンプトに顔文字を表示するようにした

--- zsh/conf.d/prompt.zsh
+++ zsh/conf.d/prompt.zsh
@@ -8,9 +8,22 @@ zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
 zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
+
+status_code () {
+  local color face
+
+  local ok="(*'-')ｲｲｶﾝｼﾞ"
+  local ng="(;^q^%)ｱﾋﾞｬｰ "
+
+  color="%(?.${fg[green]}.${fg[blue]})"
+  face="%(?!$ok <!$ng <)"
+
+  echo "${color}${face}${reset_color}"
+}
+
 # %F{色} 任意のテキスト %fで、%fまで色を変更できる
 PROMPT='%F{003}%*%f %F{magenta}%n%f@%F{blue}%M%f %F{200}[%~]%f${vcs_info_msg_0_}
-%# '
+`status_code` %# '
 
 show_zsh_prompt_colors() {
   for c in {000..255}; do echo -n "\e[38;5;${c}m $c" ; [ $(($c%16)) -eq 15 ] && echo;done;echo
