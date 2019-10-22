514fc7e075851a70d102c2c23bb8855416002a44
jiro4989 <jiroron666@gmail.com>
2019-10-22 17:45:39 +0900

    :fire: いらない設定を削除

--- zsh/conf.d/prompt.zsh
+++ zsh/conf.d/prompt.zsh
@@ -1,20 +1,11 @@
 #!/usr/bin/env zsh
-#
-autoload -Uz vcs_info
-setopt prompt_subst
-zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
-zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
-zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
-zstyle ':vcs_info:*' actionformats '[%b|%a]'
-precmd () { vcs_info }
 
-show_zsh_prompt_colors() {
-  for c in {000..255}; do echo -n "\e[38;5;${c}m $c" ; [ $(($c%16)) -eq 15 ] && echo;done;echo
-}
+readonly __ZSH_PROMPT_CMD="$HOME/.nimble/bin/zshprompt"
 
 autoload -Uz add-zsh-hook
 _nicy_prompt() {
-  PROMPT=$("$HOME/.nimble/bin/zshprompt")
+  if type "$__ZSH_PROMPT_CMD" >& /dev/null; then
+    PROMPT=$("$__ZSH_PROMPT_CMD")
+  fi
 }
 add-zsh-hook precmd _nicy_prompt
