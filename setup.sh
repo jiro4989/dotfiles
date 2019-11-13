b1af37f92e4154891630421a4256557e679bf0d0
jiro4989 <jiroron666@gmail.com>
2019-11-13 22:19:27 +0900

    gcc不足

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eu
 
+pacman -S --no-confirm gcc
+
 curl "https://nim-lang.org/choosenim/init.sh" > init.sh
-bash init.sh -y
+sh init.sh -y
 rm init.sh
 export PATH=$PATH:$HOME/.nimble/bin
 nim --hints:off setup
