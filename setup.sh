2d771e99508633a2a5d20e8106591858917e3e5f
jiro4989 <jiroron666@gmail.com>
2019-11-14 00:31:42 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -2,11 +2,17 @@
 
 set -eu
 
-pacman -Syyu --noconfirm gcc
+pacman -Syyu --noconfirm gcc git
+
+cd /tmp
+mkdir work
+cd $_
 
 curl "https://nim-lang.org/choosenim/init.sh" > init.sh
 sh init.sh -y
 rm init.sh
 export PATH=$PATH:$HOME/.nimble/bin
 
+git clone https://github.com/jiro4989/dotfiles
+cd dotfiles
 nim --hints:off setup
