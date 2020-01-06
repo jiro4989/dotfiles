2303cfd9d74cf982b82583f9e4aade3f43c6e0f6
jiro4989 <jiroron666@gmail.com>
2020-01-06 20:56:34 +0900

    tmux.conf

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ set -eux
 ln -sfn "$PWD/.config/termite" "$HOME/.config/"
 ln -sfn "$PWD/.config/i3" "$HOME/.config/"
 ln -sfn "$PWD/.config/Code" "$HOME/.config/"
+ln -sfn "$PWD/.tmux.conf" "$HOME/"
