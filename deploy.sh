e504e5538022fd4ef2c40e234fc0e462890035b4
jiro4989 <jiroron666@gmail.com>
2020-01-06 21:02:27 +0900

    vim

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ set -eux
 ln -sfn "$PWD/.config/termite" "$HOME/.config/"
 ln -sfn "$PWD/.config/i3" "$HOME/.config/"
 ln -sfn "$PWD/.config/Code" "$HOME/.config/"
+ln -sfn "$PWD/.config/nvim" "$HOME/.config/"
 ln -sfn "$PWD/.tmux.conf" "$HOME/"
+ln -sfn "$PWD/.vim" "$HOME/"
