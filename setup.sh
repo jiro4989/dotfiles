515a298186cb8e522d045e172665f3a5e4981523
jiro4989 <jiroron666@gmail.com>
2020-01-06 21:44:22 +0900

    WIP

--- setup.sh
+++ setup.sh
@@ -2,17 +2,13 @@
 
 set -eu
 
-pacman -Syyu --noconfirm gcc git
+pacman -Syyu --noconfirm git
 
 cd /tmp
 mkdir work
 cd $_
 
-curl "https://nim-lang.org/choosenim/init.sh" > init.sh
-sh init.sh -y
-rm init.sh
-export PATH=$PATH:$HOME/.nimble/bin
-
 git clone https://github.com/jiro4989/dotfiles
 cd dotfiles
-nim --hints:off setup
+./init.sh
+./deploy.sh
