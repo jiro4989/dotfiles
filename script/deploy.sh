15e8bfca537bf501ae385331e714ef1ae7a2ed52
jiro4989 <jiroron666@gmail.com>
2020-02-23 02:22:05 +0900

    コンフィグ系をconf配下に移動

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,21 +2,26 @@
 
 set -eux
 
-readonly CONFIG_DIR="$PWD/.config"
+readonly CONFIG_DIR="$PWD/conf"
+readonly DOT_CONFIG_DIR="$CONFIG_DIR/.config"
+readonly DST_DOT_CONFIG_DIR="$HOME/.config"
 
-mkdir -p "$CONFIG_DIR"
-ln -sfn "$CONFIG_DIR/termite" "$HOME/.config/"
-ln -sfn "$CONFIG_DIR/i3" "$HOME/.config/"
-ln -sfn "$CONFIG_DIR/Code" "$HOME/.config/"
-ln -sfn "$CONFIG_DIR/nvim" "$HOME/.config/"
-#ln -sfn "$CONFIG_DIR/fish/config.fish" "$HOME/.config/fish/config.fish"
-ln -sfn "$PWD/.tmux.conf" "$HOME/"
+# .config
+mkdir -p "$DST_DOT_CONFIG_DIR/"
+ln -sfn "$DOT_CONFIG_DIR/termite" "$DST_DOT_CONFIG_DIR/"
+ln -sfn "$DOT_CONFIG_DIR/i3" "$DST_DOT_CONFIG_DIR/"
+ln -sfn "$DOT_CONFIG_DIR/Code" "$DST_DOT_CONFIG_DIR/"
+ln -sfn "$DOT_CONFIG_DIR/nvim" "$DST_DOT_CONFIG_DIR/"
+#ln -sfn "$DOT_CONFIG_DIR/fish/config.fish" "$DST_DOT_CONFIG_DIR/fish/config.fish"
+
+# home/.*config
+ln -sfn "$CONFIG_DIR/.tmux.conf" "$HOME/"
 if [[ -d "$HOME/.vim" ]]; then
   rm -rf "$HOME/.vim"
 fi
-ln -sfn "$PWD/.vim" "$HOME/"
-ln -sfn "$PWD/.zshrc" "$HOME/"
-ln -sfn "$PWD/.gitconfig" "$HOME/"
+ln -sfn "$CONFIG_DIR/.vim" "$HOME/"
+ln -sfn "$CONFIG_DIR/.zshrc" "$HOME/"
+ln -sfn "$CONFIG_DIR/.gitconfig" "$HOME/"
 
 # 会社用にリポジトリで管理しないスクリプトが存在するときだけ実行する
 if [[ -f secrets.sh ]]; then
