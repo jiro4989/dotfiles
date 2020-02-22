05bcdc804a5f038454cff0b03040c4e554d71af9
jiro4989 <jiroron666@gmail.com>
2020-02-23 02:01:30 +0900

    外部ファイルからパッケージをインストールするように変更

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -22,9 +22,8 @@ exists_dir() {
 install_pkgs() {
   local task=$1
   shift
-  local pkgs=("$@")
 
-  for pkg in "${pkgs[@]}"; do
+  while read -r pkg; do
     "func_$task" "$pkg" 2>&1 >> "$log_file"
     ret=$?
 
@@ -63,71 +62,6 @@ setup_go() {
   )
 }
 
-readonly APT_PKGS=(
-  chromium-browser
-  curl
-  direnv
-  dnsutils
-  docker
-  docker-compose
-  fish
-  git
-  guake
-  indent
-  krita
-  lmms
-  nodejs
-  npm
-  python3
-  python3-pip
-  ruby
-  shellcheck
-  steam
-  supervisor
-  systemd
-  tmux
-  vim
-  wine
-  winetricks
-  zsh
-)
-
-readonly GO_PKGS=(
-  "github.com/motemen/ghq"
-  "golang.org/x/tools/cmd/godoc"
-  "golang.org/x/tools/gopls"
-  "golang.org/x/tools/cmd/gorename"
-  "github.com/atotto/clipboard/cmd/gocopy"
-  "github.com/derekparker/delve/cmd/dlv"
-  "github.com/hhatto/gocloc/cmd/gocloc"
-  "github.com/mvdan/sh/cmd/shfmt"
-  "github.com/peco/peco/cmd/peco/..."
-  "github.com/ChimeraCoder/gojson/gojson"
-  "github.com/mattn/twty"
-  "github.com/greymd/ojichat"
-  "github.com/jiro4989/textimg"
-  "github.com/xztaityozx/owari"
-  "github.com/jiro4989/align"
-)
-
-# GHQ
-readonly GHQ_PKGS=(
-  "jiro4989/dotfiles"
-  "jiro4989/sandbox"
-  "jiro4989/text-proofreading"
-  "jiro4989/scripts"
-  "jiro4989/clitools"
-  "unkontributors/super_unko"
-  "theoremoon/ShellgeiBot"
-  "theoremoon/ShellgeiBot-Image"
-)
-
-# pip
-readonly PIP_PKGS=(
-  "python-language-server"
-  "pynvim"
-)
-
 echo -e "✨ updating packages ..."
 sudo apt-get update -yqq
 
@@ -141,12 +75,12 @@ git config --global ghq.root ~/src
 git config --global alias.preq pull-request
 git config --global alias.see browse
 
-install_pkgs apt "${APT_PKGS[@]}"
+install_pkgs apt < pkg/ubuntu
 setup_go
 source $PWD/.zsh/conf.d/env.zsh
-install_pkgs go "${GO_PKGS[@]}"
-install_pkgs ghq "${GHQ_PKGS[@]}"
-install_pkgs pip "${PIP_PKGS[@]}"
+install_pkgs go < pkg/go
+install_pkgs ghq < pkg/ghq
+install_pkgs pip < pkg/pip
 ./setup_font.sh
 
 sudo add-apt-repository -y ppa:lazygit-team/release
