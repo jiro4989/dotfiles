052c6473ad922f9dceaff6a45f2ec182481cb401
jiro4989 <jiroron666@gmail.com>
2020-02-25 23:47:14 +0900

    デプロイスクリプトを修正

--- script/deploy.sh
+++ script/deploy.sh
@@ -12,12 +12,13 @@ ln -sfn "$DOT_CONFIG_DIR/termite" "$DST_DOT_CONFIG_DIR/"
 ln -sfn "$DOT_CONFIG_DIR/i3" "$DST_DOT_CONFIG_DIR/"
 ln -sfn "$DOT_CONFIG_DIR/Code" "$DST_DOT_CONFIG_DIR/"
 ln -sfn "$DOT_CONFIG_DIR/nvim" "$DST_DOT_CONFIG_DIR/"
-#ln -sfn "$DOT_CONFIG_DIR/fish/config.fish" "$DST_DOT_CONFIG_DIR/fish/config.fish"
+ln -sfn "$DOT_CONFIG_DIR/fish/config.fish" "$DST_DOT_CONFIG_DIR/fish/config.fish"
 
 # home/.*config
 ln -sfn "$CONFIG_DIR/.tmux.conf" "$HOME/"
 if [[ -d "$HOME/.vim" ]]; then
-  rm -rf "$HOME/.vim"
+  unlink "$HOME/.vim" || true
+  rm -rf "$HOME/.vim" || true
 fi
 ln -sfn "$CONFIG_DIR/.vim" "$HOME/"
 ln -sfn "$CONFIG_DIR/.zshrc" "$HOME/"
