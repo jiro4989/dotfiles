63c8c66beb550515e1d211966715c92697ba52cc
jiro4989 <jiroron666@gmail.com>
2020-04-24 09:24:46 +0900

    最新git

--- script/init_ubuntu.sh
+++ script/init_ubuntu.sh
@@ -128,3 +128,8 @@ sudo ./linux-install-1.10.1.536.sh
 
 # Clojureワンライナー
 bash <(curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install)
+
+# 最新Git
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt update -y
+sudo apt install -y git
