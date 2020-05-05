5d923b298f11f983eb8c3e7fdb84bc5cb04bff97
jiro4989 <jiroron666@gmail.com>
2020-05-05 14:04:40 +0000

    ファイルを追加したときに自動で反映されるように変更

--- setup.sh
+++ setup.sh
@@ -8,22 +8,36 @@ readonly DST_DOT_CONFIG_DIR="$HOME/.config"
 
 # .config
 mkdir -p "$DST_DOT_CONFIG_DIR/"
-ln -sfn "$DOT_CONFIG_DIR/termite" "$DST_DOT_CONFIG_DIR/"
-ln -sfn "$DOT_CONFIG_DIR/i3" "$DST_DOT_CONFIG_DIR/"
-ln -sfn "$DOT_CONFIG_DIR/Code" "$DST_DOT_CONFIG_DIR/"
-ln -sfn "$DOT_CONFIG_DIR/nvim" "$DST_DOT_CONFIG_DIR/"
+for dir in "$DOT_CONFIG_DIR/"*; do
+  # fishだけリンクの仕方が違うのでスキップ
+  base="$(basename "$dir")"
+  if [[ "$base" =~ ^(fish|\.{1,2})$ ]]; then
+    continue
+  fi
+
+  ln -sfn "$dir" "$DST_DOT_CONFIG_DIR/"
+done
+
+# fish
 mkdir -p "$DST_DOT_CONFIG_DIR/fish"
 ln -sfn "$DOT_CONFIG_DIR/fish/config.fish" "$DST_DOT_CONFIG_DIR/fish/config.fish"
 
-# home/.*config
-ln -sfn "$CONFIG_DIR/.tmux.conf" "$HOME/"
+# $HOME/.*rc
+# Vimは最初にディレクトリが作られてる場合があるので削除
 if [[ -d "$HOME/.vim" ]]; then
-  unlink "$HOME/.vim" || true
   rm -rf "$HOME/.vim" || true
 fi
-ln -sfn "$CONFIG_DIR/.vim" "$HOME/"
-ln -sfn "$CONFIG_DIR/.zshrc" "$HOME/"
-ln -sfn "$CONFIG_DIR/.gitconfig" "$HOME/"
+
+# . で始まるファイル/ディレクトリをシンボリックリンク
+for dir in "$CONFIG_DIR/".*; do
+  # . ディレクトリはスキップ
+  base="$(basename "$dir")"
+  if [[ "$base" =~ ^(\.config|\.{1,2})$ ]]; then
+    continue
+  fi
+
+  ln -sfn "$dir" "$HOME/"
+done
 
 # 会社用にリポジトリで管理しないスクリプトが存在するときだけ実行する
 if [[ -f secrets.sh ]]; then
