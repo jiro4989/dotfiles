0c3c1fe87d1dff4952eb4d0c391eaf862c6a2cf4
jiro4989 <jiroron666@gmail.com>
2020-08-01 15:04:56 +0000

    feat: Windows用の環境構築スクリプトを追加

---
+++ setup.ps1
@@ -0,0 +1,10 @@
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+iwr -useb get.scoop.sh | iex
+scoop bucket add extras
+
+scoop install discord
+scoop install slack
+scoop install vscode
+scoop install keepass
+scoop install krita
+scoop install steam
