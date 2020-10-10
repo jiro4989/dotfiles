8d1f35abfa966642b576e6c4e53fe9a0ebcf0de8
jiro4989 <jiroron666@gmail.com>
2020-10-10 09:30:30 +0900

    feat: /snap/binにPATHを通すようにした

--- conf/.config/fish/config.fish
+++ conf/.config/fish/config.fish
@@ -71,6 +71,7 @@ alias clip='xsel --clipboard --input'
 set -x JAVA_HOME /opt/java/current
 set -x EDITOR nvim
 
+set PATH /snap/bin $PATH
 for dir in /usr/local/go/bin "$HOME/bin" "$HOME/.nimble/bin" "$HOME/go/bin" "$JAVA_HOME/bin" "$HOME/.cargo/bin"
   if test -d "$dir"
     set PATH $PATH "$dir"
