6622c89deb1d696813b2bfad3cd7d3bd158a3ba1
jiro4989 <jiroron666@gmail.com>
2020-12-24 12:56:51 +0900

    fix scrpt

--- add_vm.ps1
+++ add_vm.ps1
@@ -3,19 +3,21 @@ echo "Creating a new VM from backup VM file."
 $wsl_dir = "c:\Users\jiro4989\wsl"
 $external_storage = "D:\backup\wsl"
 
-Get-Item "$external_storage"
+ls "$external_storage"
 
 $vm_file = Read-Host "Enter a VM file"
 if ($vm_file -lt 5) {
-  echo "vm_file was too short."
+  echo "[ERR] vm_file was too short."
   exit
 }
 $base_file = "$external_storage\$vm_file"
 
+wsl -l -v
+
 $distro = Read-Host "Enter a new distribution name"
 if ($distro -lt 5) {
-  echo "distro was too short."
+  echo "[ERR] distro was too short."
   exit
 }
 
-echo "wsl.exe --import $distro $wsl_dir\$distro $base_file"
+echo "wsl --import $distro $wsl_dir\$distro $base_file"
