e466324ab4eb1b3924127fa2b7ec50ff347c63c0
jiro4989 <jiroron666@gmail.com>
2020-12-24 09:31:11 +0900

    fix

--- backup_vm.ps1
+++ backup_vm.ps1
@@ -3,7 +3,7 @@ echo "Saving backups of WSL2 Virtual machine ..."
 $external_storage = "D:\backup\wsl"
 $distr = "Ubuntu-20.04"
 $today = Get-Date -UFormat "%Y-%m-%d"
-$out_file = $external_storage\${distr}_${today}.tar
+$out_file = "$external_storage\${distr}_${today}.tar"
 
 wsl --export $distr $out_file
 
