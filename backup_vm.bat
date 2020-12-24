bab6d0bcf103d2837d4eeaefd4c661c6d5f81027
jiro4989 <jiroron666@gmail.com>
2020-12-24 09:27:52 +0900

    バックアップ用のスクリプトをbat経由で起動できるように

---
+++ backup_vm.bat
@@ -0,0 +1,5 @@
+@echo off
+
+powershell -NoProfile -ExecutionPolicy Unrestricted .\backup_vm.ps1
+
+pause
--- backup_vm.ps1
+++ backup_vm.ps1
@@ -1,5 +1,10 @@
+echo "Saving backups of WSL2 Virtual machine ..."
+
 $external_storage = "D:\backup\wsl"
 $distr = "Ubuntu-20.04"
 $today = Get-Date -UFormat "%Y-%m-%d"
+$out_file = $external_storage\${distr}_${today}.tar
 
-wsl --export $distr $external_storage\${distr}_${today}.tar
+wsl --export $distr $out_file
+
+echo "Successfully saving backups: path = $out_file"
