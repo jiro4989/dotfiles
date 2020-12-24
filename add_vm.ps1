166322224bb260522ec16857493ed84cf9cb9a88
jiro4989 <jiroron666@gmail.com>
2020-12-24 13:06:17 +0900

    fix script

--- add_vm.ps1
+++ add_vm.ps1
@@ -24,6 +24,7 @@ echo "wsl --import $distro $wsl_dir\$distro $base_file"
 
 $yes = Read-Host "Create? [y/n]"
 if ($yes = "y") {
+  mkdir $wsl_dir
   wsl --import $distro $wsl_dir\$distro $base_file
   wsl -l -v
   echo "Successfully creating VM: distribution = $distro"
