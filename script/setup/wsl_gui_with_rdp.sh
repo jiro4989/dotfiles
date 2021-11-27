8221533b117ccf67282044144dc71a6ad8d00ee4
jiro4989 <jiroron666@gmail.com>
2021-11-27 16:53:36 +0900

    add script

---
+++ script/setup/wsl_gui_with_rdp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eux
+
+apt update -y
+apt upgrade -y
+apt install -y xfce4 xrdp
+
+cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
+sed -i \
+  -e 's/3389/3390/g' \
+  -e 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' \
+  -e 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' \
+  /etc/xrdp/xrdp.ini
+echo xfce4-session > ~/.xsession
+
+cp /etc/xrdp/startwm.sh /etc/xrdp/startwm.sh.bak
+sed -i \
+  -e 's_^test -x /etc/X11/Xsession.*_# &_' \
+  -e 's_^exec /bin/sh /etc/X11/Xsession.*_# &_' \
+  /etc/xrdp/startwm.sh
+echo startxfce4 >> /etc/xrdp/startwm.sh
+diff -u /etc/xrdp/startwm.sh.bak /etc/xrdp/startwm.sh
