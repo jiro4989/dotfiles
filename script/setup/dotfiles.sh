4fccaa9cd6f9a868e89bf3753c9bc8e39bace4c4
jiro4989 <jiroron666@gmail.com>
2021-11-27 17:47:34 +0900

    move

--- script/setup/dotfiles.sh
+++ script/setup/dotfiles.sh
@@ -8,5 +8,5 @@ readonly DOTFILES_BRANCH=${DOTFILES_BRANCH:-master}
 cd
 git clone -b "${DOTFILES_BRANCH}" https://github.com/jiro4989/dotfiles
 cd dotfiles
-./link_config.sh
+./script/setup/link_config.sh
 relma install -f ./conf/releases.json
