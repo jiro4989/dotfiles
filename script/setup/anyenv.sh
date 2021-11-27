9e1bc68bf8fc2bb49fab338a374949b1d0aa323d
jiro4989 <jiroron666@gmail.com>
2021-11-27 18:19:21 +0900

    fix

--- script/setup/anyenv.sh
+++ script/setup/anyenv.sh
@@ -5,21 +5,17 @@ set -eux
 readonly NODE_VERSION=16.9.1
 
 git clone https://github.com/riywo/anyenv ~/.anyenv
-export PATH=$PATH:$HOME/.anyenv/bin/anyenv
+export PATH=$PATH:$HOME/.anyenv/bin
+mkdir -p "$(anyenv root)/plugins"
+yes | anyenv install --init
+git clone https://github.com/znz/anyenv-update.git "$(anyenv root)/plugins/anyenv-update"
 
-# 何故かパスが通らず...
-anyenv=$HOME/.anyenv/bin/anyenv
-
-mkdir -p "$($anyenv root)/plugins"
-yes | $anyenv install --init
-git clone https://github.com/znz/anyenv-update.git "$($anyenv root)/plugins/anyenv-update"
-
-$anyenv install nodenv
-eval "$($anyenv init -)"
+anyenv install nodenv
+eval "$(anyenv init -)"
 nodenv install $NODE_VERSION
 nodenv global $NODE_VERSION
 
 npm i -g bash-language-server
 
-$anyenv install pyenv
+anyenv install pyenv
 pip3 install --user cfn-lint
