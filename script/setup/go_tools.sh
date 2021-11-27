807b527adc51512850cb8afe837fcc8487edfdf8
jiro4989 <jiroron666@gmail.com>
2021-11-27 20:30:08 +0900

    fix: goへPATHを通した

--- script/setup/go_tools.sh
+++ script/setup/go_tools.sh
@@ -3,7 +3,7 @@
 set -eux
 
 cd /tmp
-export PATH=$PATH:$HOME/go/bin
+export PATH=$PATH:$HOME/go/bin:/usr/local/go/bin
 export GO111MODULE=on
 go get golang.org/x/tools/gopls@latest
 go get golang.org/x/tools/cmd/goimports@latest
