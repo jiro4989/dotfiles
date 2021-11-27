59fa4414777ffae195228b6af65654a9bd37cbd3
jiro4989 <jiroron666@gmail.com>
2021-11-27 18:51:18 +0900

    fix

--- script/setup/anyenv.sh
+++ script/setup/anyenv.sh
@@ -3,6 +3,7 @@
 set -eux
 
 readonly NODE_VERSION=16.9.1
+readonly PYTHON_VERSION=3.9.9
 
 git clone https://github.com/riywo/anyenv ~/.anyenv
 export PATH=$PATH:$HOME/.anyenv/bin
@@ -18,4 +19,8 @@ nodenv global $NODE_VERSION
 npm i -g bash-language-server
 
 anyenv install pyenv
-pip3 install --user cfn-lint
+eval "$(anyenv init -)"
+pyenv install $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+eval "$(anyenv init -)"
+pip install --user cfn-lint
