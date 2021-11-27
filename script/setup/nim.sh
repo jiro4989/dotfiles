4793eb66b88cb249fce00cc9e2a79d847bd7055b
jiro4989 <jiroron666@gmail.com>
2021-11-27 18:43:14 +0900

    fix

--- script/setup/nim.sh
+++ script/setup/nim.sh
@@ -5,6 +5,8 @@ set -eux
 mkdir -p /tmp/work
 cd /tmp/work
 
+export PATH=$PATH:$HOME/.nimble/bin
+
 curl https://nim-lang.org/choosenim/init.sh -sSf > init.sh
 bash init.sh -y
 nimble install -Y inim bump subnet nimjson
