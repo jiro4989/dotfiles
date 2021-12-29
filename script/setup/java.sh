57d76ee7c75d16c5c6d32542741fc729e1f16ffd
jiro4989 <jiroron666@gmail.com>
2021-12-29 17:25:35 +0900

    feat: jdk17をインストールするように変更

--- script/setup/java.sh
+++ script/setup/java.sh
@@ -5,10 +5,10 @@ set -eux
 mkdir -p /tmp/work
 cd /tmp/work
 
-curl -O https://download.java.net/java/GA/jdk16.0.1/7147401fd7354114ac51ef3e1328291f/9/GPL/openjdk-16.0.1_linux-x64_bin.tar.gz
+curl -O https://download.java.net/openjdk/jdk17/ri/openjdk-17+35_linux-x64_bin.tar.gz
+tar xvf openjdk-17+35_linux-x64_bin.tar.gz
 
-java_version=16.0.1
-tar xvf openjdk-${java_version}_linux-x64_bin.tar.gz
+java_version=17
 mkdir -p /opt/java
 mv jdk-${java_version} /opt/java/
 ln -sfn /opt/java/jdk-${java_version} /opt/java/current
