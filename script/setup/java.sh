8bdeec0937681df402493b427a30faf0693ad315
jiro4989 <jiroron666@gmail.com>
2022-01-25 07:49:52 +0900

    java upgrade

--- script/setup/java.sh
+++ script/setup/java.sh
@@ -4,9 +4,11 @@ set -eux
 
 mkdir -p /tmp/work
 cd /tmp/work
-curl -sSLO "https://github.com/adoptium/temurin16-binaries/releases/download/jdk-16.0.2%2B7/OpenJDK16U-jdk_x64_linux_hotspot_16.0.2_7.tar.gz"
-tar xf OpenJDK16U-jdk_x64_linux_hotspot_16.0.2_7.tar.gz
+curl -sSLO "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_linux_hotspot_17.0.1_12.tar.gz"
+tar xf OpenJDK17U-jdk_x64_linux_hotspot_17.0.1_12.tar.gz
+
+dir=jdk-17.0.1+12
 
 mkdir -p /opt/java
-mv jdk-16.0.2+7/ /opt/java/
-ln -sfn /opt/java/jdk-16.0.2+7/ /opt/java/current
+mv $dir/ /opt/java/
+ln -sfn /opt/java/$dir/ /opt/java/current
