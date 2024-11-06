84a9353f0669da0fcc189ea9d8dfb8ace6cedbde
jiro4989 <jiroron666@gmail.com>
2024-11-07 00:20:11 +0900

    add: fish_prompt スクリプトを追加

---
+++ conf/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,15 @@
+function fish_prompt
+  set -l ret $status
+
+  echo (set_color yellow)(date +"%Y-%m-%dT%H:%M:%S") (set_color cyan)(prompt_pwd)(set_color magenta)(__fish_git_prompt)
+  set -l user_prompt "%"
+  if test $USER = "root"
+    set user_prompt "#"
+  end
+
+  set -l prompt (set_color green)"(*'-')!"(set_color white)
+  if test $ret -ne 0
+    set prompt (set_color red)"(;T_T)?"(set_color white)
+  end
+  echo "$prompt < $user_prompt "
+end
