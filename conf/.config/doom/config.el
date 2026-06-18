db0997bdc940e789ff3a5b596c3e397a2cfced9c
jiro4989 <jiroron666@gmail.com>
2026-06-18 19:28:42 +0900

    feat: MacOS のときとそれ以外でフォントを切りかえるように変更

--- conf/.config/doom/config.el
+++ conf/.config/doom/config.el
@@ -21,8 +21,13 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Noto Sans Mono CJK JP" :size 17 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Noto Sans Mono CJK JP" :size 18))
+(if (eq system-type 'darwin)
+    ;; MacOS 用の設定
+    (setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 17 :weight 'semi-light)
+          doom-variable-pitch-font (font-spec :family "Hack Nerd Font Mono" :size 18))
+  ;; MacOS 以外用の設定
+  (setq doom-font (font-spec :family "Noto Sans Mono CJK JP" :size 17 :weight 'semi-light)
+        doom-variable-pitch-font (font-spec :family "Noto Sans Mono CJK JP" :size 18)))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
